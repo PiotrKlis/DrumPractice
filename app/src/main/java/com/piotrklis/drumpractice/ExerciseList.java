f71f11106bf0a96ff60e0c94b52f5a804d5557a9
PK <piotr.klis0@gmail.com>
2018-05-30 00:25:14 +0200

    Added ExerciseList fields

--- app/src/main/java/com/piotrklis/drumpractice/ExerciseList.java
+++ app/src/main/java/com/piotrklis/drumpractice/ExerciseList.java
@@ -9,4 +9,34 @@ public class ExerciseList {
     List<Exercise> exerciseList;
     String name;
 
+    public ExerciseList(Exercise exercise, List<Exercise> exerciseList, String name) {
+        this.exercise = exercise;
+        this.exerciseList = exerciseList;
+        this.name = name;
+    }
+
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public List<Exercise> getExerciseList() {
+        return exerciseList;
+    }
+
+    public void setExerciseList(List<Exercise> exerciseList) {
+        this.exerciseList = exerciseList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
