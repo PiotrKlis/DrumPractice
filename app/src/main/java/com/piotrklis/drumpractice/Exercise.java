11a382d28d2dc9f8852b94c6eb39665ba1d565b4
PK <piotr.klis0@gmail.com>
2018-05-29 22:35:45 +0200

    Obiekty, bottom navigation

--- app/src/main/java/com/piotrklis/drumpractice/Exercise.java
+++ app/src/main/java/com/piotrklis/drumpractice/Exercise.java
@@ -1,5 +1,15 @@
 package com.piotrklis.drumpractice;
 
 
+import android.media.Image;
+
 public class Exercise {
+
+    long overallTime;
+    int exercisesDone;
+    int name;
+    int nextExerciseName;
+    long timeLeft;
+    Image image;
+
 }
---
+++ app/src/main/java/com/piotrklis/drumpractice/ExerciseList.java
@@ -0,0 +1,12 @@
+package com.piotrklis.drumpractice;
+
+
+import java.util.List;
+
+public class ExerciseList {
+
+    Exercise exercise;
+    List<Exercise> exerciseList;
+    String name;
+
+}
