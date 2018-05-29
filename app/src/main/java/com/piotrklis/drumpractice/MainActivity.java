03537e92f01e17968fe8e36bbeb966ac91483642
PK <piotr.klis0@gmail.com>
2018-05-29 21:13:14 +0200

    View added fired by the activity

--- app/src/main/java/com/piotrklis/drumpractice/MainActivity.java
+++ app/src/main/java/com/piotrklis/drumpractice/MainActivity.java
@@ -2,6 +2,7 @@ package com.piotrklis.drumpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -12,25 +13,16 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.editText_main_overallTime)
-    EditText overallTimeTimer;
-    @BindView(R.id.textView_main_exercisesDone)
-    TextView exercisesDoneCount;
-    @BindView(R.id.textView_main_nextExerciseName)
-    TextView nextExerciseName;
-    @BindView(R.id.textView_main_currentExerciseName)
-    TextView currentExerciseName;
-    @BindView(R.id.editText_main_timeLeft)
-    EditText timeLeftTimer;
-    @BindView(R.id.imageView_main_exercise)
-    ImageView exerciseImage;
-    @BindView(R.id.imageButton_main_power)
-    ImageButton powerButton;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        view = new ExcerciseView(this);
+        setContentView(view);
     }
+
+
+
 }
